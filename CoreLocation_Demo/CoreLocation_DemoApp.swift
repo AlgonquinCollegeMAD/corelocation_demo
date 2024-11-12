50cf58ac65f313f2ab2ad1421efc6fc3a8755854
Vladimir Cezar <vladimircezar@icloud.com>
2024-11-12 16:59:39 -0500

    Initial Commit

---
+++ CoreLocation_Demo/CoreLocation_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreLocation_DemoApp.swift
+//  CoreLocation_Demo
+//
+//  Created by Vladimir Cezar on 2024.11.12.
+//
+
+import SwiftUI
+
+@main
+struct CoreLocation_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
